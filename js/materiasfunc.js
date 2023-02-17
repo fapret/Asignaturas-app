6872181eedbefa54feea1695950072dbb7563a8c
Santiago Nicolas Diaz Conde <sndc.33@gmail.com>
2023-02-16 23:02:56 -0300

    Actualizar js/materiasfunc.js

--- js/materiasfunc.js
+++ js/materiasfunc.js
@@ -145,15 +145,16 @@ function getBitFieldShrinked(bitField){
   for(let i = 0; i < bitField.length; i++){
     let localChar = 0;
     let j;
+    let pos = 0;
     for(j = i; j < 5 && j < bitField.length; j++){
-      let pos = j - i;
+      pos = j - i;
       let value = bitField.charAt(j);
       if(value == "1")
         localChar |= Math.pow(2, pos);
       else
         localChar &= ~Math.pow(2, pos);
     }
-    i += j;
+    i += pos;
     out += String.fromCharCode(';'.charCodeAt(0) + localChar);
   }
   return out;
